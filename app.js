ab98c6b0c9d01e55331928496404330604571d25
drnaylayousuf <naylayousuf.dr1310@gmail.com>
2023-09-12 03:37:28 +0500

    js 2 assignment

---
+++ app.js
@@ -0,0 +1,301 @@
+// chapter 6 (math expression)
+//  Q1 ANSWER
+
+// 1.   x++;     or
+// 2.   ++x;
+
+//  Q2 ANSWER
+
+//  var x = 100;
+//  var result = --x;
+//  console.log(result);
+  
+//  Q3 ANSWER 
+//     ( answer is 51)
+//    var x = 50;
+//    var y = x++;
+//    var result= x++;
+//    console.log(result);
+                       
+//  Q4 ANSWER
+//     (answer 49)
+// var y = 50;
+// var z = --y;
+// var result = z;
+// console.log(result);
+
+// Q5 ANSWER
+
+// var num= 10;
+// var  newNum = num--;
+// console.log(newNum);
+
+// Q6  ANSWER
+
+// Assume num is the original variable
+// var num = 5;
+// var newNum =  num++;
+// console.log(newNum);
+
+
+// Q7 ANSWER
+// var x=10;
+// var y= x++;
+// // var result= y;
+// alert(x);
+
+
+
+// CHAPTER 7 (MATH EXPRESSION lll)
+
+// Q1 ANSWER
+
+// var calculatedNum= 2+(2*6);
+// // ruff work  (12+2 =14 answer)
+// console.log(calculatedNum);
+
+// Q2  ANSWER
+
+// var calculatedNum = (2 + 2) * 6;
+//     ruff work (4*6 =24 answer)
+// console.log(calculatedNum);
+
+// Q3  ANSWER
+
+// var calculatedNum = (2 + 2) * (4 + 2);
+// //  ruff work (4*6= 24 answer)
+// console.log(calculatedNum);
+
+// Q4  ANSWER
+
+// var calculatedNum =((2+2) *4) +2 ;
+// // ((4)*4)+2
+// //  ruff work (16+2 =18 answer)
+//  console.log(calculatedNum);
+
+// Q5  ANSWER
+
+// clarified with parentheses, producing 56
+//  var price = (2+2) * (4+10);
+// console.log(price);
+
+// Q6  ANSWER
+
+// clarified with parentheses, producing 20
+// var units = 2 + (2 * 4) + 10;
+// console.log(units);
+
+
+// Q7  ANSWER
+
+// clarified with parentheses, producing 5
+// var pressure = 4 / (2 * 4);
+// console.log(pressure);
+
+
+// CHAPTER 8  (Concatenating Text Strings)
+
+// Q1 ANSWER
+//      (concatenating text)
+
+// var num = "2" + "2";
+// console.log(num);
+
+//  (22) answer   //  concatenation
+
+
+//  Q2  ANSWER
+
+// var message = ("Hello" + "Dolly");
+// alert(message);
+
+// another way
+// var firstName= "Hello";
+// var lastName= "Dolly";
+// var message =firstName  + lastName;
+// alert(message);
+
+//  Q3  ANSWER
+
+// alert("33" + 3);
+//  (333) answer   //  concatenation
+
+//  Q4  ANSWER
+
+// alert("Pakistan" + "Zindabad");
+
+// another way
+// var firstName= "Pakistan";
+// var lastName= "Zindabad";
+// var message =firstName + lastName;
+// alert(message);
+
+
+//  Q5  ANSWER
+
+// Assume  that x is a variable
+
+// var x = "Hello" + 42;
+
+// console.log(x);
+
+//  Q6  ANSWER
+
+// var a = "Hello";
+// var b = "World";
+// var  c = a + b;
+// console.log(c);
+
+
+//  CHAPTER 9 (Prompts)
+
+// Q1 ANSWER
+
+// var firstName = prompt("Enter your  first name"); 
+// console.log(firstName);
+
+
+// Q2 ANSWER
+
+//  var region =  prompt("Enter your country?", "china");
+
+//    another way
+//  var country = prompt("Country? ", "China");
+
+
+// // Q3 ANSWER
+
+// correct this statement
+
+// var yourName = prompt("Enter your name");
+
+
+// // Q4 ANSWER
+
+// var name = prompt("What is your name?", "Aliya");
+
+// // Q5 ANSWER
+
+// var shade1 ="What is your favourite colour?";
+// var shade2   = "purple"; 
+// var  shade3 = prompt(shade1, shade2);
+
+//     another one
+
+// // var first=prompt("enter city");
+// // var second=prompt("karachi");
+// // var respond=prompt(first,second);
+
+
+
+// // Q6 ANSWER
+
+// var place  = prompt("What is your favourite place?", " italy" );
+// alert("Good morning," + place);
+
+
+// CHAPTER 10  (if statments)
+
+// Q1 ANSWER
+
+// var city = "karachi";
+// if(city == "karachi"){
+//   console.log("The city of Lights");
+// }
+
+// another way
+
+// var city= "karachi"
+// if(city=== "karachi") {
+//   console.log("The City OF Lights")
+// }
+
+// Q2 ANSWER
+    //  
+                        //  
+// var x=prompt("enter x value");
+// var y=prompt("enter y value");
+// if (x === y) {
+//     var z=prompt("enter value of z");
+// alert(z);
+// }
+
+
+
+
+// Q3 ANSWER
+
+// Assume ZipCode is a variable
+
+//  var ZipCode = prompt("What is your ZipCode");
+//  if (ZipCode == "10010") {
+//   alert("Karachi");
+// } else {
+//   alert("Please write correct city");
+// }
+
+
+// // Q4 ANSWER
+
+//
+                    //    
+// // // Initial value of the variable
+// // var x = 1;
+
+// // // Test whether x has a particular numerical value
+// // if (x === 1) {
+// //   // If x is equal to 1, assign a new value to x
+// //   x = 2;
+// // } else {
+// //   // If x is not equal to 1, do something else
+// //   // You can add code here for other cases if needed
+// // }
+
+// // // Display the updated value of x
+// // console.log("x is now:", x);
+
+
+    // Chapter 11 
+
+// (Comparison
+    //     Operators)
+    
+//     Q1   ANSWER
+//     var number=+prompt("enter number");
+//     if(number !== 4){
+
+//     }
+
+
+//    Q2   ANSWER
+// var number=+prompt("enter number");
+// //     if(number >= 4){
+
+// //     }
+
+
+//    Q3   ANSWER
+// var number=+prompt("enter number");
+// //     if(number !== 4){
+// number=4
+// //     }
+
+
+//   Q4    ANSWER
+// var number=+prompt("enter number");
+// //     if(number !== 4){
+// alert("congratulations")
+// //     }
+
+
+//   Q5   ANSWER
+// var first=prompt("enter firstname");
+// if(first !== "bushra"){
+//     alert("mismatch")
+//     //     }
+
+
+
+
+
+// ........THE END...........
